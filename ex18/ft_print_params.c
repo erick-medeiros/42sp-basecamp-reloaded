f48a66703c7dc5e1d72cd14d0518f2ef667d2809
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-03-28 00:50:17 +0200

    feat: ex18 finalizado

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/28 00:22:00 by eandre-f          #+#    #+#             */
+/*   Updated: 2022/03/28 00:50:01 by eandre-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	if (argc == 1)
+		return (1);
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(&argv[i][0]);
+		ft_putstr("\n");
+		i++;
+	}
+	return (1);
+}
