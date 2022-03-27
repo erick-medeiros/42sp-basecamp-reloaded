e6cef350fb4ca894fc2992bf005a71840f92ed93
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-03-27 05:38:10 +0200

    feat: ex06 finalizado

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/27 05:23:30 by eandre-f          #+#    #+#             */
+/*   Updated: 2022/03/27 05:35:55 by eandre-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		ft_putchar(alpha);
+		alpha++;
+	}
+}
