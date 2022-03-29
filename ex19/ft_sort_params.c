7885f58cab92cd4cb06d3ec4b072372576f7bef3
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-03-29 17:12:12 +0200

    feat: ex19 finalizado

---
+++ ex19/ft_sort_params.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/28 01:03:08 by eandre-f          #+#    #+#             */
+/*   Updated: 2022/03/29 17:10:45 by eandre-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	n;
+
+	n = 0;
+	while (s1[n] && s2[n] && (s1[n] == s2[n]))
+		n++;
+	return (s1[n] - s2[n]);
+}
+
+void	ft_sort(int size, char **tab)
+{
+	char	*tmp;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i < size)
+	{
+		j = i;
+		while (j < size)
+		{
+			if (ft_strcmp(tab[i], tab[j]) > 0)
+			{
+				tmp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	argc--;
+	argv++;
+	ft_sort(argc, argv);
+	i = 0;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putstr("\n");
+		i++;
+	}
+	return (1);
+}
