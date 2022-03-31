5a2aa6d282cf0fa89fe6fba97eaa4c7161cc3f40
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-03-31 22:17:37 +0200

    fix:farquivos com bytes zerados

---
+++ ex00/tar.sh
@@ -0,0 +1,24 @@
+# tar -xf exo.tar
+clear
+mkdir test0 test2
+touch test1 test3 test4
+ln test3 test5
+ln -s test0 test6
+echo "000" > test1
+echo "" > test3
+echo "0" > test4
+touch -t 202106012047 test0
+touch -t 202106012146 test1
+touch -t 202106012245 test2
+touch -t 202106012344 test3
+touch -t 202106012343 test4
+touch -ht 202106012220 test6
+chmod 715 test0
+chmod 714 test1
+chmod 504 test2
+chmod 404 test3
+chmod 641 test4
+ls -l --time-style=full-iso
+rm exo.tar
+tar -cf exo.tar *
+rm -rf test*
